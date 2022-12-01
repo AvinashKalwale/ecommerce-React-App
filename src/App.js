3b3c6af4b9a7c913bc0321e47468eebe3f17d79d
Avinash Kalwale <avinashkalwale12@gmail.com>
2022-12-01 17:44:04 +0530

    UI added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>React Photo Search </h1>
+    <div>
+    <h1>React Photo Search </h1>
+    <button>Bookmark</button>
+    </div>
+     
      <div>
      <input placeholder='search for high resolution images'/>
      <button>Search</button>
