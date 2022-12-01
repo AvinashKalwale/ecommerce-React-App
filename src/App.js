a1e43b23af7bcf3ea1f59650feeddc1b312b492d
Avinash Kalwale <avinashkalwale12@gmail.com>
2022-12-01 19:40:53 +0530

    search images add using unsplash

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-
+import React, {useState} from 'react';
+// import { BrowserRouter,Route, Routes} from 'react-router-dom';
 import './App.css';
+import axios from 'axios';
+// 3import { useState } from 'react';
 
 function App() {
+  const [images , setImages]=useState("")
+  const [result , setResult]=useState([])
+  const changeImages=()=>{
+   axios.get(`https://api.unsplash.com/search/photos?page=1&query=${images}&client_id=IJhVVp-eiBp38IwvsXy9SRsPrKjAfUqlb9khIt5Oowg`)
+    .then((response)=>{
+      console.log(response.data)
+      setResult(response.data.results)
+    })  
+  }
   return (
     <div className="App">
     <div id='head'>
@@ -10,8 +22,18 @@ function App() {
     </div>
      
      <div>
-     <input placeholder='search for high resolution images'/>
-     <button id='search'>Search</button>
+     <input type="text" value={images}
+     onChange={(e)=>{
+      setImages(e.target.value)
+     }} placeholder='search for high resolution images'/>
+     <button type='submit' onClick={changeImages} id='search'>Search</button>
+     </div>
+     <div>
+      {result.map((value)=>{
+        return <div>
+          <img src={value.urls.small}/>
+        </div>
+      })}
      </div>
     </div>
   );
